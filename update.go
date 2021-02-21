2121edae9cf507df60297bfe4f43517792e3e08c
Jeff Culverhouse <jeff@graystorm.com>
2021-02-21 09:30:25 +0000

    rework when-to-update EOD pricing code

--- update.go
+++ update.go
@@ -46,28 +46,40 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 //  if we don't have the EOD price for the prior business day
 //  OR if we don't have it for the current business day and it's now 7pm or later
 func updateTicker(ticker *Ticker) (*Ticker, error) {
-	EasternTZ, _ := time.LoadLocation("America/New_York")
-	currentTime := time.Now().In(EasternTZ)
-	priorWeekDay := currentTime.AddDate(0, 0, -1)
-	for ; priorWeekDay.Weekday() == 0 || priorWeekDay.Weekday() == 6; priorWeekDay.AddDate(0, 0, -1) {
-	}
+	mostRecentDaily, err := getDailyMostRecent(ticker.Ticker_id)
+  if err != nil {
+    mylog.Warning.Printf("Error getting most recent EOD date for %s(%d): %s", ticker.Ticker_symbol, ticker.Ticker_id, err)
+    return ticker, err
+  }
+  mostRecentDailyDate := mostRecentDaily.Price_date
+  mostRecentAvailable := mostRecentEODPricesAvailable()
 
-	time_now := currentTime.Format("15:04:05")
-	date_now := currentTime.Format("2006-01-02")
-	is_today_weekday := (currentTime.Weekday() > 0 && currentTime.Weekday() < 6)
-
-	date_prior := priorWeekDay.Format("2006-01-02")
-	most_recent, err := getDailyMostRecent(ticker.Ticker_id)
-
-	// if we don't have most recent EOD prices up to the previous workday, get em
-	// or, if we don't have today's AND today is a weekday AND it's past 7PM, get em
-	if err == nil && (most_recent.Price_date < date_prior || (most_recent.Price_date < date_now && is_today_weekday && time_now > "19:00:00")) {
-		mylog.Warning.Printf("Use Marketstack API to get the latest EOD price info for %s", ticker.Ticker_symbol)
+	if mostRecentDailyDate < mostRecentAvailable {
+		mylog.Info.Printf("Using Marketstack API to get the latest EOD price info for %s(%d)", ticker.Ticker_symbol, ticker.Ticker_id)
 		ticker, err = updateMarketstackTicker(ticker.Ticker_symbol)
-		if err == nil {
-			mylog.Info.Printf("%s updated with latest EOD prices", ticker.Ticker_symbol)
-		}
+		if err != nil {
+      mylog.Warning.Print("Error getting EOD prices for %s(%d): %s", ticker.Ticker_symbol, ticker.Ticker_id, err)
+      return ticker, err
+    }
+		mylog.Info.Printf("%s(%d) updated with latest EOD prices", ticker.Ticker_symbol, ticker.Ticker_id)
 	}
 
-	return ticker, err
+	return ticker, nil
 }
+
+func mostRecentEODPricesAvailable() string {
+	currentDateTime := time.Local
+	currentTime := currentDateTime.Format("15:04:05")
+	currentDate := currentDateTime.Format("2006-01-02")
+  isWorkDay := mytime.isWorkday(currentDateTime)
+
+  if isWorkDay && currentTime > "19:00:00" {
+    return currentDate
+  }
+
+  prevWorkDate := mytime.lastWorkDate(currentDateTime)
+  prevWorkDay := prevWorkDate.Format("2006-01-02")
+
+  return prevWorkDay
+}
+
